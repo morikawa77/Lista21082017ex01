92ef32d9a58966fe8baa09ea5a54e9b9d4bb7956
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 07:26:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lista21082017ex01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int idadeAnos, idadeMeses, idadeDias, idadeDiasTotal;
+			Console.WriteLine("Digite a quantidade de anos, meses e dias da sua idade: ");
+			idadeAnos = Convert.ToInt32(Console.ReadLine());
+			idadeMeses = Convert.ToInt32(Console.ReadLine());
+			idadeDias = Convert.ToInt32(Console.ReadLine());
+			idadeDiasTotal = (idadeAnos * 365) + (idadeMeses * 30) + idadeDias;
+            Console.Write("Sua idade em dias é: {0} dias.", idadeDiasTotal);
+			Console.ReadLine();
+        }
+    }
+}
